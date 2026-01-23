86b81cef4b8194438f079c4089acc99c3733a1cd
annguyen2k8 <hacker.an1901@gmail.com>
2026-01-23 15:05:34 +0700

    Refactor: add Board and Block grid system

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import numpy as np
 import pygame
-from pygame.locals import *
+from pygame.locals import QUIT
+
+from typing import Callable, Tuple
 
 pygame.init()
 
@@ -8,40 +11,56 @@ FONT = pygame.Font("./assets/font.otf")
 BLACK = (20, 20, 20)
 WHITE = (255, 255, 255)
 
-def render_text(text, color = WHITE):
+
+class Block:
+    EMPTY = 0
+    WALL = 1
+
+
+class Board:
+    def __init__(self, width: int, height: int):
+        # 2D array
+        self.arr = np.zeros((width, height), dtype=np.int64)
+
+    def foreach_block(self, func: Callable[[Tuple[int, int], int], None]):
+        for pos, block in np.ndenumerate(self.arr):
+            func(pos, int(block))
+
+
+def render_text(text, color=WHITE):
     return FONT.render(text, True, color)
 
+
 def main():
-    screen = pygame.display.set_mode((1280, 720))
+    screen = pygame.display.set_mode((640, 480))
+
     clock = pygame.Clock()
     running = True
+
+    board = Board(16, 16)
 
     pygame.display.set_icon(pygame.image.load("./assets/icon.png"))
     pygame.display.set_caption("A* Search Pygame")
 
+    def render_blocks(board: Board):
+        def render_block(pos: Tuple[int, int], block: int): ...
+
+        board.foreach_block(render_block)
+
     while running:
+        # handle event and update
         for event in pygame.event.get():
             if event.type == QUIT:
                 running = False
 
+        # draw
         screen.fill(BLACK)
-        
-        text = render_text(f"{clock.get_fps():.0f} FPS")
-        
-        greet_text = render_text("Hello world!")
 
-        screen.blit(text, (5, 5))
-            
-        screen.blit(
-            greet_text,
-            (
-                (screen.get_width() - greet_text.get_width()) * 0.5,
-                (screen.get_height() - greet_text.get_height()) * 0.5
-            )
-        )
+        render_blocks(board)
 
         pygame.display.update()
         clock.tick(60)
+
 
 if __name__ == "__main__":
     try:
