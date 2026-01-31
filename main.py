94e40354f9bc0d4a6548d1be9571cccaa9768339
annguyen2k8 <hacker.an1901@gmail.com>
2026-01-31 15:41:20 +0700

    Refactor: stop spaghetti, add proper board & game structure

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import numpy as np
 import pygame
-from pygame.locals import QUIT
+from pygame import Surface, Clock, Rect, Event, Vector2
+from pygame.locals import QUIT, VIDEORESIZE
 
-from typing import Callable, Tuple
+from enum import Enum
+
+
+from typing import Callable, List, Optional, Tuple, Union, Any
 
 pygame.init()
 
@@ -10,60 +13,192 @@ FONT = pygame.Font("./assets/font.otf")
 
 BLACK = (20, 20, 20)
 WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
 
-class Block:
+class CellType(Enum):
     EMPTY = 0
     WALL = 1
 
 
-class Board:
-    def __init__(self, width: int, height: int):
-        # 2D array
-        self.arr = np.zeros((width, height), dtype=np.int64)
+class Cell:
+    x: int
+    y: int
+    cell_type: CellType
 
-    def foreach_block(self, func: Callable[[Tuple[int, int], int], None]):
-        for pos, block in np.ndenumerate(self.arr):
-            func(pos, int(block))
+    def __init__(self, x: int, y: int, cell_type: CellType = CellType.EMPTY):
+        super().__init__()
+
+        self.x = x
+        self.y = y
+        self.cell_type = cell_type
+
+
+class Board:
+    __width: int
+    __height: int
+
+    __cells: List[Cell] = []
+
+    def __init__(self, width: int, height: int):
+        self.__width = width
+        self.__height = height
+
+        for x in range(width):
+            for y in range(height):
+                self.__cells.append(Cell(x, y))
+
+    @property
+    def size(self) -> Tuple[int, int]:
+        return (self.width, self.height)
+
+    @property
+    def width(self) -> int:
+        return self.__width
+
+    @property
+    def height(self) -> int:
+        return self.__height
+
+    @property
+    def cells(self) -> List[Cell]:
+        return self.__cells
+
+    def foreach_cell(self, func: Callable[[Cell], None]):
+        for cell in self.cells:
+            func(cell)
+
+    def is_inside(self, x: int, y: int) -> bool:
+        return 0 <= x < self.width and 0 <= y < self.height
+
+    def get_cell(self, x: int, y: int) -> Optional[Cell]:
+        for cell in self.cells:
+            if cell.x == x and cell.y == y:
+                return cell
+        return None
 
 
 def render_text(text, color=WHITE):
     return FONT.render(text, True, color)
 
 
-def main():
-    screen = pygame.display.set_mode((640, 480))
+class Game:
+    board_padding: int = 8
 
-    clock = pygame.Clock()
-    running = True
+    screen: Surface
+    clock: Clock
+    running: bool
 
-    board = Board(16, 16)
+    board_surface: Surface
+    board_rect: Rect
+    board: Board
 
-    pygame.display.set_icon(pygame.image.load("./assets/icon.png"))
-    pygame.display.set_caption("A* Search Pygame")
+    cell_size: float
 
-    def render_blocks(board: Board):
-        def render_block(pos: Tuple[int, int], block: int): ...
+    def __init__(self):
+        self.screen = pygame.display.set_mode((1280, 720), pygame.RESIZABLE)
 
-        board.foreach_block(render_block)
+        self.clock = Clock()
+        self.running = True
 
-    while running:
-        # handle event and update
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                running = False
+        self.board = Board(16, 10)
 
-        # draw
-        screen.fill(BLACK)
+        pygame.display.set_icon(pygame.image.load("./assets/icon.png"))
+        pygame.display.set_caption("A* Search Pygame")
 
-        render_blocks(board)
+        self.update_render()
 
-        pygame.display.update()
-        clock.tick(60)
+    @property
+    def screen_rect(self) -> Rect:
+        return self.screen.get_rect()
+
+    def mainloop(self):
+        while self.running:
+            self.clock.tick(60)
+
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    self.running = False
+
+                if event.type == VIDEORESIZE:
+                    self.update_render()
+
+            self.update()
+
+            self.draw()
+
+            pygame.display.update()
+
+    def update(self):
+        pygame.display.set_caption(f"Tile pos: {self.get_mouse_tile()}")
+        pass
+
+    def update_render(self):
+        viewport = self.screen_rect
+        padding = self.board_padding
+
+        usable_width = viewport.width - padding * 2
+        usable_height = viewport.height - padding * 2
+
+        self.tile_size = min(
+            usable_width / self.board.width,
+            usable_height / self.board.height,
+        )
+
+        board_width = self.tile_size * self.board.width
+        board_height = self.tile_size * self.board.height
+
+        self.board_rect = Rect(
+            padding + (usable_width - board_width) / 2,
+            padding + (usable_height - board_height) / 2,
+            board_width,
+            board_height,
+        )
+
+        self.board_surface = Surface((self.board_rect.width, self.board_rect.height))
+
+    def get_mouse_tile(self) -> Optional[Tuple[int, int]]:
+        mouse_pos = Vector2(pygame.mouse.get_pos())
+
+        if not self.board_rect.collidepoint(mouse_pos):
+            return None
+
+        local_pos = mouse_pos - self.board_rect.topleft
+
+        return (
+            int(local_pos.x / self.tile_size),
+            int(local_pos.y / self.tile_size),
+        )
+
+    def rect_tile_at(self, x: int, y: int) -> Rect:
+        return Rect(
+            self.tile_size * x,
+            self.tile_size * y,
+            self.tile_size,
+            self.tile_size,
+        )
+
+    def draw(self):
+        self.screen.fill(BLACK)
+
+        def draw_cell(cell: Cell):
+            pygame.draw.rect(
+                self.board_surface,
+                BLUE,
+                self.rect_tile_at(cell.x, cell.y),
+                1,
+            )
+
+        self.board.foreach_cell(draw_cell)
+
+        self.screen.blit(self.board_surface, self.board_rect)
 
 
 if __name__ == "__main__":
+    game = Game()
     try:
-        main()
+        game.mainloop()
     except Exception as error:
         raise error
